ae582122082934df9d79423f18e14936a15b463f
Henrry Cabrera <hcabrerao@unal.edu.co>
2024-03-26 18:39:49 -0500

    intento uno

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,43 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+
 import pandas as pd
+
+from datetime import datetime as dt
+
+
+def format_date(str_date):
+    try:
+        return dt.strptime(str_date, "%d/%m/%Y")
+    except ValueError:
+        return dt.strptime(str_date, "%Y/%m/%d")
 
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    #
-    # Inserte su código aquí
-    #
+    for col in df.columns:
+        if df[col].dtype == "object":
+            df[col] = df[col].str.lower()
+
+    df.idea_negocio = df.idea_negocio.str.replace("[_-]", " ", regex=True)
+
+    df.barrio = df.barrio.str.replace("[-]", " ", regex=True)
+    df.barrio = df.barrio.str.replace("[ ]+", "_", regex=True)
+
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+
+    df.fecha_de_beneficio = df.fecha_de_beneficio.apply(format_date)
+
+    df.monto_del_credito = df.monto_del_credito.replace(
+        "[\,$]|(\.00$)", "", regex=True
+    ).astype(float)
+
+    df.línea_credito = df.línea_credito.str.replace("[-_]", " ", regex=True)
+
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis=0, inplace=True)
 
     return df
